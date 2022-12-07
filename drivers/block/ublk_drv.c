a23aedc89496a936a9f966fa1f2623ff97ee8f34
Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>
2022-12-07 15:40:44 +0800

    bpf: add UBLK program type
    Normally, userspace block device implementations need to copy data between
    kernel block layer's io requests and userspace block device's userspace
    daemon. For example, ublk and tcmu both have similar logic, but this
    operation will consume cpu resources obviously, especially for large io.
    
    There are methods trying to reduce these cpu overheads, then userspace
    block device's io performance will be improved further. These methods
    contain: 1) use special hardware to do memory copy, but seems not all
    architectures have these special hardware; 2) software methods, such as
    mmap kernel block layer's io requests's data to userspace daemon [1],
    but it has page table's map/unmap, tlb flush overhead, security issue,
    etc, and it maybe only friendly to large io.
    
    To solve this problem, I'd propose a new method, which will combine the
    respective advantages of io_uring and ebpf. Add a new program type
    BPF_PROG_TYPE_UBLK for ublk, userspace block device daemon process will
    register ebpf progs, which will use bpf helper offered by ublk bpf prog
    type to submit io requests on behalf of daemon process in kernel, note
    io requests will use kernel block layer io reqeusts's pages to do io,
    then the memory copy overhead will be gone.
    
    [1] https://lore.kernel.org/all/20220318095531.15479-1-xiaoguang.wang@linux.alibaba.com/
    
    Signed-off-by: Xiaoguang Wang <xiaoguang.wang@linux.alibaba.com>

--- drivers/block/ublk_drv.c
+++ drivers/block/ublk_drv.c
@@ -43,6 +43,8 @@
 #include <asm/page.h>
 #include <linux/task_work.h>
 #include <uapi/linux/ublk_cmd.h>
+#include <linux/filter.h>
+#include <linux/bpf.h>
 
 #define UBLK_MINORS		(1U << MINORBITS)
 
@@ -186,6 +188,27 @@ static wait_queue_head_t ublk_idr_wq;	/* wait until one idr is freed */
 static DEFINE_MUTEX(ublk_ctl_mutex);
 
 static struct miscdevice ublk_misc;
+
+static const struct bpf_func_proto *
+ublk_bpf_func_proto(enum bpf_func_id func_id, const struct bpf_prog *prog)
+{
+	return bpf_base_func_proto(func_id);
+}
+
+static bool ublk_bpf_is_valid_access(int off, int size,
+			enum bpf_access_type type,
+			const struct bpf_prog *prog,
+			struct bpf_insn_access_aux *info)
+{
+	return false;
+}
+
+const struct bpf_prog_ops bpf_ublk_prog_ops = {};
+
+const struct bpf_verifier_ops bpf_ublk_verifier_ops = {
+	.get_func_proto		= ublk_bpf_func_proto,
+	.is_valid_access	= ublk_bpf_is_valid_access,
+};
 
 static void ublk_dev_param_basic_apply(struct ublk_device *ub)
 {
